53e9d9aa01aac96cb69f83c46d69cf3b7d1ddce7
hope awaited <hope awaited>
2017-03-29 19:12:54 +0200

    commit 3

--- app.js
+++ app.js
@@ -121,10 +121,10 @@ app.post('/webhook/', function (req, res) {
 	   console.log(event.message);
 	   facebookUserInfo(senderID,tokenHTC,function(last_name,first_name){});
 	   
-	   if(event.message.quick_reply == "CREATE_USER" && event.message.quick_reply){
+	   if(event.message.quick_reply.payload == "CREATE_USER" && event.message.quick_reply){
 		   console.log("OK");
 	   }
-		if(event.message.quick_reply == "NO_CREATE_USER" && event.message.quick_reply){
+		if(event.message.quick_reply.payload == "NO_CREATE_USER" && event.message.quick_reply){
 			console.log("NO");
 		}
 	   
