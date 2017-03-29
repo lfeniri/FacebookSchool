675f0c22ac033e27f2db64a6bc3c37280bbd140d
hope awaited <hope awaited>
2017-03-29 22:06:25 +0200

    commit 3

--- index.js
+++ index.js
@@ -6,22 +6,6 @@ var Quiz = require("./Quiz");
 
 var DB = require("nosql");
 var quiz = DB.load('./DB/quiz.nosql');
-/*
-quiz.find().make(function(filter) {
-    filter.where('id', '=', 2);
-	filter.callback(function(err,response) {
-		 console.log(response[0]);
-		});
-	});
- */
- 
-//Teacher.getTeacher("2354",function(res){res.print();});
-Student.getStudent("2344",function(res){if(res == undefined)console.log("NO ONE");console.log("OK = "res);});
-//t.print();
-/*
-var teacher1 = new Teacher("2354","Yacine");
-teacher1.addCourse(1);
-teacher1.addCourse(2);
-teacher1.addCourse(3);
-teacher1.print();
-teacher1.save();*/
+
+var s = new Student(123,"test","test",[]);
+s.save();
