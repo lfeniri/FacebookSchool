78c87e7a36c5ca7950c357ab8ae737ad3fbd0a49
hope awaited <hope awaited>
2017-03-30 20:31:52 +0200

    commit 3

--- Student.js
+++ Student.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 model = mongoose.model('student',{_id:String,last_name:String,first_name:String,courses:[Number]});
-console.log(mongoose.connection.readyState);
-//mongoose.connect('mongodb://school:Lounes1993@ds029456.mlab.com:29456/school');
+mongoose.connect('mongodb://school:Lounes1993@ds029456.mlab.com:29456/school',function(error){
+    if(error) console.log("ERREUR :: " + error);
+	console.log("connection successful");
+});
 
 function Student(id,last_name,first_name,courses){
 	this.id = id;
--- Teacher.js
+++ Teacher.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 model = mongoose.model('teacher',{_id:String,last_name:String,first_name:String,courses:[Number]});
-mongoose.connect('mongodb://school:Lounes1993@ds029456.mlab.com:29456/school');
+mongoose.connect('mongodb://school:Lounes1993@ds029456.mlab.com:29456/school',function(error){
+    if(error) console.log("ERREUR :: " + error);
+	console.log("connection successful");
+});
 
 function Teacher(id,last_name,first_name,courses){
 	this.id = id;
