9b0c2313ac5da98a9abb150949bb34f3dc7e46de
hope awaited <hope awaited>
2017-03-30 17:01:42 +0200

    commit 3

--- Student.js
+++ Student.js
@@ -14,7 +14,7 @@ Student.model = function(){
 }
 
 Student.prototype.save = function() {
-	mongoose.connect('mongodb://school:Lounes@ds029456.mlab.com:29456/school');
+	mongoose.connect('mongodb://school:Lounes1993@ds029456.mlab.com:29456/school');
 	var m = mongoose.model('student',{id:String,last_name:String,first_name:String,courses:[Number]});
 	var std = new m({id:this.id,last_name:this.last_name,first_name:this.first_name,courses:[]});
 	std.save(function (err) {
