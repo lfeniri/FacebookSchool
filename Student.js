2ea047802535c9f1d0fed2c324fe101d70661a99
hope awaited <hope awaited>
2017-03-31 01:28:38 +0200

    commit 3

--- Student.js
+++ Student.js
@@ -43,6 +43,7 @@ Student.getAllStudents = function(callback){
           console.log(err);
         } else{
             console.log('list of user ;');
+			console.log('students');
 			
         }
     }); 
